ec23993224958f6113eba7afd6e2b876c869834c
Dorcas-Amoo <doorcars@gmail.com>
2020-07-09 03:48:57 +0000

    Started adding overlay and updated comments

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
-//The codes below were largely influenced by tutorials from personal research and adapted to achieve my desired outcome, please see references//
+/*The codes below were largely influenced by tutorials from personal research and adapted to achieve my desired outcome, please refer to references in the README.md, thank you. */
 
+/*------ For card flips when clicked---- Credits to Marina-Ferreira */
 const cards = document.querySelectorAll('.card');
 
 function cardFlip() {
@@ -7,3 +8,5 @@ function cardFlip() {
 }
 
 cards.forEach(card => card.addEventListener('click', cardFlip));
+
+/*------ For Overlay Contents---- Credits to  */
