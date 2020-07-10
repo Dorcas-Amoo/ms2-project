061fc18d3c0a986aca8495448e52505200df225c
Dorcas-Amoo <doorcars@gmail.com>
2020-07-10 00:28:07 +0000

    Declared the first and second card when flipped

--- scripts.js
+++ scripts.js
@@ -1,7 +1,10 @@
 /*The codes below were largely influenced by tutorials from personal research and adapted to achieve my desired outcome, please refer to references in the README.md, thank you. */
 
-/*------ For card flips when clicked---- Credits to Marina-Ferreira */
+/*------ For card flips and storage when clicked---- Credits to Marina-Ferreira */
 const cards = document.querySelectorAll('.card');
+
+let cardIsFlipped = false;
+let cardOne, cardTwo;
 
 function cardFlip() {
   this.classList.toggle('flip');
@@ -10,3 +13,5 @@ function cardFlip() {
 cards.forEach(card => card.addEventListener('click', cardFlip));
 
 /*------ For Overlay Contents---- Credits to  */
+
+
